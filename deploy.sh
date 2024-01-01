555c21f5c3d63447b9e14cfe5b5e6bac98f5183f
Markus Petters <mdpetter@ncsu.edu>
2024-01-01 10:11:10 -0800

    deploy script for cee233

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 julia -e 'using Franklin; optimize()'
-cd ../deploy/cee200/
+cd ../deploy/cee233/
 git checkout gh-pages
-cp -r ../../cee200/__site/* .
+cp -r ../../cee233/__site/* .
 git add --all
 git commit -m "update gh-pages"
 git push -u origin gh-pages
-cd ../../cee200
+cd ../../cee233
